d385b65c87c7a3d3d513947b603ec0cebcb9d024
SYED MUHARRAM ALI <syedmuharramali7@gmail.com>
2026-09-07 04:31:51 +0500

    fix(client): clear API cache on sign out

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,12 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authReducer from '../features/auth/authSlice'
+import { configureStore, createListenerMiddleware } from '@reduxjs/toolkit'
+import authReducer, { logout } from '../features/auth/authSlice'
 import { baseApi } from '../features/api/baseApi'
+
+const listenerMiddleware = createListenerMiddleware()
+
+listenerMiddleware.startListening({
+  actionCreator: logout,
+  effect: async (_action, listenerApi) => {
+    listenerApi.dispatch(baseApi.util.resetApiState())
+  },
+})
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     [baseApi.reducerPath]: baseApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().prepend(listenerMiddleware.middleware).concat(baseApi.middleware),
   devTools: import.meta.env.DEV,
 })
