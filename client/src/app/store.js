5d5814213f5e9fef9e5fa0371d980aaeb954f9c3
SYED MUHARRAM ALI <syedmuharramali7@gmail.com>
2026-09-06 19:36:17 +0500

    feat(client): register RTK Query store middleware

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
+import { baseApi } from '../features/api/baseApi'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
   devTools: import.meta.env.DEV,
 })
